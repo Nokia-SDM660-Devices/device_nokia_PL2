7294689ea4f64c97f0fcd1ab51e1b65a6c2700fd
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-01-21 14:37:22 +0530

    PL2: Add powerhint.json for sdm660
    [SebaUbuntu]: Make a copy of the config with the following commit applied:
        twolip: configs: Adapt powerhint to sdm636
    
        Change-Id: I1d36f08bfa9e85f80872d7de0794e5091ed33d0e
    
    Co-authored-by: Davide Garberi <dade.garberi@gmail.com>
    Change-Id: If270fc906fd833bb6b0d3b00621c0b079346e47d

--- sdm660.mk
+++ sdm660.mk
@@ -352,6 +352,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.0.vendor \
     android.hardware.power.stats@1.0-service.mock
 
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
