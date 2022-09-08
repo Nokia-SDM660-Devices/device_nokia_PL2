680e13ba91b4dc3ba30c0689ddb35cc41d93082d
Bruno Martins <bgcngm@gmail.com>
2022-09-08 12:03:17 +0100

    sdm660-common: Opt-out for LLVM binutils
    Change-Id: I353dfb264191e587620b4a88f1f6783e54395d6e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,6 +91,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 TARGET_KERNEL_CONFIG := nokia_defconfig
 TARGET_KERNEL_SOURCE := kernel/nokia/sdm660
+TARGET_KERNEL_LLVM_BINUTILS := false
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
