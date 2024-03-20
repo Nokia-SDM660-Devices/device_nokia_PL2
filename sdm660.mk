3489713ed095fc7e3a401185028964da3c3b5728
basamaryan <basam.aryan@gmail.com>
2024-03-20 01:16:12 -0400

    PL2: Don't explicitly include updatable_apex.mk
    It is inherited by base_system.mk as of QPR2
    
    Change-Id: I020a05b233f61ea28d03199cbc76051e6e97e1dc

--- sdm660.mk
+++ sdm660.mk
@@ -16,9 +16,6 @@
 
 DEVICE_PATH := device/nokia/PL2
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/nokia/PL2/PL2-vendor.mk)
 
