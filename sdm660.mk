dd22aebb73460f8f55858d8c25698f8bc0b3ea0a
Michael Bestas <mikeioannina@gmail.com>
2024-06-14 00:26:49 +0300

    PL2: Build missing libraries for 14 QPR3
    Change-Id: I6a84526ca1e9f4e5140c8068646433cab3fe85b2

--- sdm660.mk
+++ sdm660.mk
@@ -216,7 +216,8 @@ PRODUCT_PACKAGES += \
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0.vendor
+    android.hardware.gatekeeper@1.0.vendor \
+    libion.vendor
 
 # FM
 PRODUCT_PACKAGES += \
@@ -309,6 +310,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
     android.system.net.netd@1.1.vendor \
+    libnetutils.vendor \
     netutils-wrapper-1.0
 
 # NFC
@@ -373,6 +375,8 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     librmnetctl \
+    libsqlite.vendor \
+    libsysutils.vendor \
     libxml2
 
 # Ramdisk
