11db6db75bda905d13fa5543b7e2fb64a90eada5
Ubuntu <batman@ubuntuzyx.pm0gy4k0gmteldfin30gehnm0f.hx.internal.cloudapp.net>
2024-07-05 21:31:47 +0000

    sdm660: inherit GCam
    Signed-off-by: Ubuntu <batman@ubuntuzyx.pm0gy4k0gmteldfin30gehnm0f.hx.internal.cloudapp.net>

--- sdm660.mk
+++ sdm660.mk
@@ -164,6 +164,9 @@ PRODUCT_PACKAGES += \
     libgui_shim \
     libgui_shim_vendor
 
+PRODUCT_PACKAGES += \
+    Camera
+
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
@@ -221,9 +224,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
     libqti_vndfwk_detect.vendor
-
-# Google Camera
-$(call inherit-product, vendor/nokia/Camera/Camera-vendor.mk)
 
 # GPS
 PRODUCT_PACKAGES += \
@@ -414,6 +414,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH) \
+    vendor/nokia/Camera \
     hardware/samsung
 
 # Spatial Audio
