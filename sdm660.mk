d472c3f670406b0fd75e3300b98fa573afb60b8e
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2024-07-06 17:07:50 +0000

    PL2: spatial audio: Parse correctly the headtracking sensor xml
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -419,8 +419,10 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Spatial Audio
 PRODUCT_PACKAGES += \
-    frameworks/native/data/etc/android.hardware.sensor.dynamic.head_tracker.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.dynamic.head_tracker.xml \
     libspatialaudio
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.sensor.dynamic.head_tracker.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.dynamic.head_tracker.xml
 
 # Spatial Audio: optimize spatializer effect
 PRODUCT_PROPERTY_OVERRIDES += \
