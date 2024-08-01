084be7190ea4decbd19b86b427cad06b62dcd94e
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2024-08-01 10:47:25 +0000

    PL2: Add `Dolby Atmos`
    repo - https://github.com/userariii/vendor_motorola-dolby.git
    
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,7 @@ git clone https://github.com/Nokia-SDM660-Devices/vendor_nokia_PL2.git vendor/no
 git clone https://github.com/Nokia-SDM660-Devices/kernel_nokia_sdm660.git kernel/nokia/sdm660 -b fourteen --depth=1
 
 # Dolby
-git clone https://github.com/userariii/hardware_Motorola_Dolby.git hardware/Motorola/Dolby -b v1.1-daxappui
+git clone -b v1.2 https://github.com/userariii/vendor_motorola-dolby.git vendor/motorola-dolby
 
 # Camera
 git clone -b main https://gitlab.com/userariii/vendor_nokia_googlecamera.git vendor/nokia/GoogleCamera
