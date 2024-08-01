cac7cf2c0c1e991cf7d270b622f2e92a158a17f4
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2024-08-01 10:47:25 +0000

    PL2: Add `Dolby Atmos` support for Nokia SDM660 [1/2]
    repo - https://github.com/userariii/vendor_motorola-dolby.git
    
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -130,6 +130,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Audio (Dolby Atmos)
+$(call inherit-product, vendor/nokia/dolby/setup.mk)
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0 \
