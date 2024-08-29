03b3a0a83510db04fc4fd939db14a80654c1f4d0
userariii <abhattacharjee717@gmail.com>
2024-08-29 16:22:05 +0000

    PL2: lineage --> aosp

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 # vendor Tree
-git clone https://github.com/Nokia-SDM660-Devices/android_vendor_nokia_PL2.git vendor/nokia/PL2 -b android-S --depth=1
+git clone https://github.com/Nokia-SDM660-Devices/android_vendor_nokia_PL2.git vendor/nokia -b android-U --depth=1
 
 # Kernel Tree
-git clone https://github.com/Nokia-SDM660-Devices/android_kernel_nokia_sdm660.git kernel/nokia/sdm660 -b android-S --depth=1
+git clone https://github.com/Nokia-SDM660-Devices/kernel_nokia_sdm660.git kernel/nokia/sdm660 -b android-U-HMP --depth=1
