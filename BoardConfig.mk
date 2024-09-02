6bb1c5003e1ef080c4b2011de0c2a7aa23520fa5
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2024-09-02 17:38:42 +0000

    PL2: lineage --> aosp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,7 +73,7 @@ TARGET_USES_ION := true
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml \
+    vendor/aosp/config/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
