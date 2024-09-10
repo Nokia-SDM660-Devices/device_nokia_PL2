8254277312702c74470687e6b43cbea50bef2a69
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2024-09-10 21:18:55 +0000

    Revert "PL2: Switch to QTI powerHAL [SQUASH]"
    This reverts commit ca24553912c149bf7bfae50d98c09eebff82eb7a.

--- sdm660.mk
+++ sdm660.mk
@@ -334,9 +334,10 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power.stats@1.0-service.mock \
-    android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.0.vendor
+    android.hardware.power-service-qti
 
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
